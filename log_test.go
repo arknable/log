7b690da52d040266153c20863e048284bc5b9d1d
Lufty <lufty.abdillah@gmail.com>
2019-08-09 16:55:12 +0700

    added test for log file

--- log_test.go
+++ log_test.go
@@ -1,6 +1,11 @@
 package log
 
-import "testing"
+import (
+	"io"
+	"log"
+	"os"
+	"testing"
+)
 
 func TestLog(t *testing.T) {
 	Debug("This is a debug message")
@@ -14,4 +19,15 @@ func TestLog(t *testing.T) {
 	Warningf("This is a %s message", "warningf")
 	Errorf("This is an %s message", "errorf")
 	Fatalf("This is a %s message", "fatalf")
+}
+
+func TestLogFile(t *testing.T) {
+	logFile, err := os.OpenFile("test.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	SetOutput(io.MultiWriter(os.Stdout, logFile))
+	TestLog(t)
 }
