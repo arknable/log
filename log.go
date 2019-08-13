3ca2bab2d48ea53705853f93d22c834db4abb944
Lufty <lufty.abdillah@gmail.com>
2019-08-13 19:29:31 +0700

    restructured for multiple instance support

--- log.go
+++ log.go
@@ -1,143 +1,75 @@
 package log
 
-import (
-	"fmt"
-	"io"
-	"log"
-	"os"
-	"sync"
+import "io"
 
-	"github.com/fatih/color"
-)
-
-var (
-	colorDebug        = color.New(color.Italic).SprintFunc()
-	colorInfo         = color.New(color.Reset).SprintFunc()
-	colorWarning      = color.New(color.Bold).SprintFunc()
-	colorError        = color.New(color.Bold).SprintFunc()
-	colorFatal        = color.New(color.Bold).SprintFunc()
-	messageFormat     = " -- %15s: %v"
-	unformattedFormat = "%s\n"
-	logger            *log.Logger
-	writer            io.Writer
-	lock              sync.Mutex
-	queue             []*message
-)
-
-type message struct {
-	IsFormatted bool
-	Format      string
-	Level       string
-	Message     string
-}
-
-// Debugf prints debug message with given format
-func Debugf(format string, v ...interface{}) {
-	printf(colorDebug("DEBUG"), format, v...)
-}
-
-// Debug prints debug message
-func Debug(v ...interface{}) {
-	Debugf(unformattedFormat, v...)
-}
-
-// Infof prints info message with given format
-func Infof(format string, v ...interface{}) {
-	printf(colorInfo("INFO"), format, v...)
-}
-
-// Info prints info message
-func Info(v ...interface{}) {
-	Infof(unformattedFormat, v...)
-}
-
-// Warningf prints warning message with given format
-func Warningf(format string, v ...interface{}) {
-	printf(colorWarning("WARNING"), format, v...)
-}
-
-// Warning prints Warning message
-func Warning(v ...interface{}) {
-	Warningf(unformattedFormat, v...)
-}
-
-// Errorf prints error message with given format
-func Errorf(format string, v ...interface{}) {
-	printf(colorError("ERROR"), format, v...)
-}
-
-// Error prints error message
-func Error(v ...interface{}) {
-	Errorf(unformattedFormat, v...)
-}
-
-// Fatalf prints fatal message with given format
-func Fatalf(format string, v ...interface{}) {
-	printf(colorFatal("FATAL"), format, v...)
-}
-
-// Fatal prints fatal message
-func Fatal(v ...interface{}) {
-	Fatalf(unformattedFormat, v...)
-}
-
-// SetOutput sets output writer
-func SetOutput(w io.Writer) {
-	lock.Lock()
-	defer lock.Unlock()
-	writer = w
-	if logger != nil {
-		logger.SetOutput(writer)
-	}
-}
-
-// Hold puts log messages in a queueu, make sure to call Release() to write the messages
-func Hold() {
-	queue = make([]*message, 0)
-}
-
-// Release writes queued messages.
-// If something wrong happen during message writing,
-// this function will make sure queue set to nil upon return.
-func Release() {
-	defer func() {
-		queue = nil
-	}()
-	for _, m := range queue {
-		logger.Printf(m.Format, m.Level, m.Message)
-	}
-}
-
-// Prints log message with given format and level
-func printf(level, format string, v ...interface{}) {
-	var msg *message
-	if format == unformattedFormat {
-		msg = &message{
-			Format:  messageFormat + "\n",
-			Level:   level,
-			Message: fmt.Sprint(v...),
-		}
-	} else {
-		msg = &message{
-			IsFormatted: true,
-			Format:      messageFormat,
-			Level:       level,
-			Message:     fmt.Sprintf(format, v...),
-		}
-	}
-	if queue != nil {
-		queue = append(queue, msg)
-		return
-	}
-	logger.Printf(msg.Format, msg.Level, msg.Message)
-}
-
-// New creates new logger
-func New() *log.Logger {
-	return log.New(writer, "", log.LstdFlags)
-}
+// Default logger
+var logger *Logger
 
 func init() {
-	writer = os.Stdout
 	logger = New()
+}
+
+// Debugf is bridge for Logger.Debugf of default logger
+func Debugf(format string, v ...interface{}) {
+	logger.Debugf(format, v...)
+}
+
+// Debug is bridge for Logger.Debug of default logger
+func Debug(v ...interface{}) {
+	logger.Debug(v...)
+}
+
+// Infof is bridge for Logger.Infof of default logger
+func Infof(format string, v ...interface{}) {
+	logger.Infof(format, v...)
+}
+
+// Info is bridge for Logger.Info of default logger
+func Info(v ...interface{}) {
+	logger.Info(v...)
+}
+
+// Warningf is bridge for Logger.Warningf of default logger
+func Warningf(format string, v ...interface{}) {
+	logger.Warningf(format, v...)
+}
+
+// Warning is bridge for Logger.Warning of default logger
+func Warning(v ...interface{}) {
+	logger.Warning(v...)
+}
+
+// Errorf is bridge for Logger.Errorf of default logger
+func Errorf(format string, v ...interface{}) {
+	logger.Errorf(format, v...)
+}
+
+// Error is bridge for Logger.Error of default logger
+func Error(v ...interface{}) {
+	logger.Error(v...)
+}
+
+// Fatalf is bridge for Logger.Fatalf of default logger
+func Fatalf(format string, v ...interface{}) {
+	logger.Fatalf(format, v...)
+}
+
+// Fatal is bridge for Logger.Fatal of default logger
+func Fatal(v ...interface{}) {
+	logger.Fatal(v...)
+}
+
+// SetOutput is bridge for Logger.SetOutput of default logger
+func SetOutput(w io.Writer) {
+	logger.SetOutput(w)
+}
+
+// Hold is bridge for Logger.Hold of default logger
+func Hold() {
+	logger.Hold()
+}
+
+// Release is bridge for Logger.Release of default logger
+func Release() {
+	logger.Release()
 }
---
+++ logger.go
@@ -0,0 +1,143 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	golog "log"
+	"os"
+	"sync"
+
+	"github.com/fatih/color"
+)
+
+var (
+	colorDebug   = color.New(color.Italic).SprintFunc()
+	colorInfo    = color.New(color.Reset).SprintFunc()
+	colorWarning = color.New(color.Bold).SprintFunc()
+	colorError   = color.New(color.Bold).SprintFunc()
+	colorFatal   = color.New(color.Bold).SprintFunc()
+
+	messageFormat     = " -- %15s: %v"
+	unformattedFormat = "%s\n"
+)
+
+type message struct {
+	IsFormatted bool
+	Format      string
+	Level       string
+	Message     string
+}
+
+// Logger is a wrapper for Go's standard logger
+type Logger struct {
+	*golog.Logger
+	lock   sync.Mutex
+	queue  []*message
+	writer io.Writer
+}
+
+// New creates new logger
+func New() *Logger {
+	return &Logger{
+		Logger: golog.New(os.Stdout, "", golog.LstdFlags),
+	}
+}
+
+// SetOutput sets output writer
+func (l *Logger) SetOutput(w io.Writer) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	l.writer = w
+	l.Logger = golog.New(l.writer, "", golog.LstdFlags)
+}
+
+// Hold puts log messages in a queueu, make sure to call Release() to write the messages
+func (l *Logger) Hold() {
+	l.queue = make([]*message, 0)
+}
+
+// Release writes queued messages.
+// If something wrong happen during message writing,
+// this function will make sure queue set to nil upon return.
+func (l *Logger) Release() {
+	defer func() {
+		l.queue = nil
+	}()
+	for _, m := range l.queue {
+		l.Printf(m.Format, m.Level, m.Message)
+	}
+}
+
+// Debugf prints debug message with given format
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.printf(colorDebug("DEBUG"), format, v...)
+}
+
+// Debug prints debug message
+func (l *Logger) Debug(v ...interface{}) {
+	l.Debugf(unformattedFormat, v...)
+}
+
+// Infof prints info message with given format
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.printf(colorInfo("INFO"), format, v...)
+}
+
+// Info prints info message
+func (l *Logger) Info(v ...interface{}) {
+	l.Infof(unformattedFormat, v...)
+}
+
+// Warningf prints warning message with given format
+func (l *Logger) Warningf(format string, v ...interface{}) {
+	l.printf(colorWarning("WARNING"), format, v...)
+}
+
+// Warning prints Warning message
+func (l *Logger) Warning(v ...interface{}) {
+	l.Warningf(unformattedFormat, v...)
+}
+
+// Errorf prints error message with given format
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.printf(colorError("ERROR"), format, v...)
+}
+
+// Error prints error message
+func (l *Logger) Error(v ...interface{}) {
+	l.Errorf(unformattedFormat, v...)
+}
+
+// Fatalf prints fatal message with given format
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	l.printf(colorFatal("FATAL"), format, v...)
+}
+
+// Fatal prints fatal message
+func (l *Logger) Fatal(v ...interface{}) {
+	l.Fatalf(unformattedFormat, v...)
+}
+
+// Prints log message with given format and level
+func (l *Logger) printf(level, format string, v ...interface{}) {
+	var msg *message
+	if format == unformattedFormat {
+		msg = &message{
+			Format:  messageFormat + "\n",
+			Level:   level,
+			Message: fmt.Sprint(v...),
+		}
+	} else {
+		msg = &message{
+			IsFormatted: true,
+			Format:      messageFormat,
+			Level:       level,
+			Message:     fmt.Sprintf(format, v...),
+		}
+	}
+	if l.queue != nil {
+		l.queue = append(l.queue, msg)
+		return
+	}
+	l.Printf(msg.Format, msg.Level, msg.Message)
+}
